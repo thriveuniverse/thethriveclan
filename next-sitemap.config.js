23abf6a63d23478323fa43d7af9fa8e9116160cb
thriveuniverse <littlehousefrance@gmail.com>
2025-08-17 20:07:34 +0200

    sitemap generation and configuration

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://thethriveclan.com',
+  generateRobotsTxt: true, // this will also generate robots.txt automatically
+}
