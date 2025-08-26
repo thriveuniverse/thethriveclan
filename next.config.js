eea122f48aa31711daa85347d9ede1c4476a219f
thriveuniverse <littlehousefrance@gmail.com>
2025-08-26 16:55:09 +0200

    removed contentlayer from next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
-module.exports = {
-  // No need for turbopack flag if using Next.js 15.4.2 default
-}
-const { withContentlayer } = require('next-contentlayer');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 };
 
-module.exports = withContentlayer(nextConfig);
+module.exports = nextConfig;
