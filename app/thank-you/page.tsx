5b0eda7dcb63668d46bcfdf9c83f9b6292a71e1f
thriveuniverse <littlehousefrance@gmail.com>
2025-09-23 19:16:43 +0200

    new thank you page

--- app/thank-you/page.tsx
+++ app/thank-you/page.tsx
@@ -1,9 +1,9 @@
-export default function THankyouPage() {
+// app/thank-you/page.tsx
+export default function ThankYouPage() {
   return (
-    <div className="p-4">
-      <h1>Email Successful!</h1>
-      <p>Thank you for your enquiry. We will get back to you shortly.</p>
-      <a href="/" className="text-blue-500 underline">Return to Home</a>
+    <div className="max-w-xl mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4">Thank you!</h1>
+      <p>Your message was received. We'll be in touch soon.</p>
     </div>
   );
 }
