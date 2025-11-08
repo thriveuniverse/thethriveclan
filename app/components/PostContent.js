011589d281832830d98ba76fe9997e26f2b38d79
thriveuniverse <littlehousefrance@gmail.com>
2025-11-08 21:01:13 +0100

    unwinded post content

--- app/components/PostContent.js
+++ app/components/PostContent.js
@@ -1,12 +1,7 @@
 "use client";
 
 import { MDXRemote } from "next-mdx-remote";
-import { Suspense } from "react";  // NEW: Built-in React (no install)
 
 export default function PostContent({ mdxSource }) {
-  return (
-    <Suspense fallback={<p className="text-center text-gray-500">Loading post...</p>}>  
-      <MDXRemote {...mdxSource} />
-    </Suspense>
-  );
+  return <MDXRemote {...mdxSource} />;
 }
